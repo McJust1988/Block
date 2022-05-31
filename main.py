1b7b9b1735fd7f509d05f9e4e69629b3129e3eb7
McJust1988 <monoflex@rambler.ru>
2022-05-31 23:10:50 +0300

    Task 1 Block 1

---
+++ main.py
@@ -0,0 +1,14 @@
+input_first_number = int(input('First_number:'))
+input_second_number = int(input('Second_number:'))
+
+if input_first_number < input_second_number:
+    print("First number is less than the second number")
+
+elif input_first_number > input_second_number:
+    print("First number is greater than the second number")
+
+#elif input_first_number >[3] input_second_number:
+    #print("First number is 3 times larger than the second number")
+
+else:
+    print("You lose")
