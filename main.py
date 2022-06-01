1642eee2d318166fb14c0d2b2033ada82cc80b13
McJust1988 <monoflex@rabmer.ru>
2022-06-01 12:56:53 +0300

    Task 1 Block 1

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# 1.Ввести произвольное число в консоле
+# Ввести пограничное число в консоле
+# Если 1ое число меньше пограничного, вывести сообщение об этом.
+# Если 1ое число больше пограничного, вывести сообщение об этом.
+# Если 1ое число больше пограничного более, чем в 3 раза, сообщить об этом.
+
 first_number = int(input('First_number:'))
 second_number = int(input('Second_number:'))
 
@@ -7,8 +13,8 @@ if first_number < second_number:
 elif first_number > second_number:
     print("First number is greater than the second number")
 
-#elif (first_number / second_number) > second_number:
-    #print("First number is 3 times larger than the second number")
+elif first_number * 3 > second_number:
+    print("First number is 3 times larger than the second number")
 
 else:
     print("Error.Try again!")
