8148028f09d82569e723cafebd311f121b2774fc
McJust1988 <monoflex@rabmer.ru>
2022-06-08 11:43:41 +0300

    Task 3 Block 2

--- 3.py
+++ 3.py
@@ -13,59 +13,59 @@ arithmetic_operation = input("Введите арифметическое дей
 
 while arithmetic_operation != "end":
     if arithmetic_operation == "+":
-        number1 = float(input("ввести 1 число: "))
-        number2 = float(input("ввести 2 число: "))
+        number1 = float(input("Введите 1 число: "))
+        number2 = float(input("Введите 2 число: "))
         print(number1 + number2)
 
     elif arithmetic_operation == "-":
-        number1 = float(input("ввести 1 число: "))
-        number2 = float(input("ввести 2 число: "))
+        number1 = float(input("Введите 1 число: "))
+        number2 = float(input("Введите 2 число: "))
         print(number1 - number2)
 
     elif arithmetic_operation == "/":
-        number1 = float(input("ввести 1 число: "))
-        number2 = float(input("ввести 2 число: "))
+        number1 = float(input("Введите 1 число: "))
+        number2 = float(input("Введите 2 число: "))
         if number2 != 0:
             print(number1 / number2)
         else:
-            print("на 0 делить нельзя")
+            print("На 0 делить нельзя")
 
     elif arithmetic_operation == "*":
-        number1 = float(input("ввести 1 число: "))
-        number2 = float(input("ввести 2 число: "))
+        number1 = float(input("Введите 1 число: "))
+        number2 = float(input("Введите 2 число: "))
         print(number1 * number2)
 
-    elif arithmetic_operation == "pow":
-        number1 = float(input("ввести 1 число: "))
-        number2 = float(input("ввести 2 число: "))
+    elif arithmetic_operation == "**":  # Возведение в степень.
+        number1 = float(input("Введите 1 число: "))
+        number2 = float(input("Введите 2 число: "))
         print(pow(number1, number2))
 
-    elif arithmetic_operation == "module":
-        number1 = float(input("ввести число: "))
+    elif arithmetic_operation == "||":  # Модуль числа.
+        number1 = float(input("Введите число: "))
         print(abs(number1))
 
-    elif arithmetic_operation == "0":  # операция рандомное число, если нажать на ноль
+    elif arithmetic_operation == "0":  # Операция рандомное число, если нажать на ноль.
         print(random.randint(0, 1000))
 
-    elif arithmetic_operation == "div":
-        number1 = float(input("ввести 1 число: "))
-        number2 = float(input("ввести 2 число: "))
+    elif arithmetic_operation == "//":  # Деление без остатка.
+        number1 = float(input("Введите 1 число: "))
+        number2 = float(input("Введите 2 число: "))
         if number2 != 0:
             print(number1 // number2)
         else:
-            print("на 0 делить нельзя")
+            print("На 0 делить нельзя")
 
-    elif arithmetic_operation == "mod":
-        number1 = float(input("ввести 1 число: "))
-        number2 = float(input("ввести 2 число: "))
+    elif arithmetic_operation == "%": # Деление по модулю (остаток от деления).
+        number1 = float(input("Введите 1 число: "))
+        number2 = float(input("Введите 2 число: "))
         print(number1 % number2)
 
-    elif arithmetic_operation == "f":
-        number1 = int(input("ввести  число: "))
+    elif arithmetic_operation == "n!":  # Факториал.
+        number1 = int(input("Введите  число: "))
         print(math.factorial(number1))
 
     elif arithmetic_operation == "arccos":
-        number1 = float(input("ввести число: "))
+        number1 = float(input("Введите число: "))
         number11 = number1 * math.pi / 180
         print(math.acos(number11))
     arithmetic_operation = input("Введите end, что бы закончить: ")
