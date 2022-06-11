f1a5539649de7e4aa330b4d379bc80fe804ca949
McJust1988 <105918886+McJust1988@users.noreply.github.com>
2022-06-11 18:19:07 +0300

    Task 6 Block 3

--- 6.2.py
+++ 6.2.py
@@ -16,9 +16,9 @@ vegetable1 = input("1 овощ: ")
 vegetable2 = input("2 овощ: ")
 vegetable3 = input("3 овощ: ")
 
-vegetables1 = int(input("сколько " + vegetable1 + ": "))
-vegetables2 = int(input("сколько " + vegetable2 + ": "))
-vegetables3 = int(input("сколько " + vegetable3 + ": "))
+vegetables1 = int(input("количество " + vegetable1 + ": "))
+vegetables2 = int(input("количество " + vegetable2 + ": "))
+vegetables3 = int(input("количество " + vegetable3 + ": "))
 
 print('{} {}({}, {})'.format(sum(vegetables1), vegetLower(vegetable1), vegetUpper(vegetable1), vegetTitle(vegetable1)))
 print('{} {}({}, {})'.format(sum(vegetables2), vegetLower(vegetable3), vegetUpper(vegetable2), vegetTitle(vegetable2)))
