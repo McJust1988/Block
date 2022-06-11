a3ebbc06665955d41dc26f9585477a71196846d6
McJust1988 <105918886+McJust1988@users.noreply.github.com>
2022-06-11 18:19:43 +0300

    Task 6 Block 3

--- 6.2.py
+++ 6.2.py
@@ -1,16 +1,20 @@
-#Переделываем задачу номер 4(про овощи) на функции.
+# Переделываем задачу номер 4(про овощи) на функции.
 
 def vegetLower(name):
     return name.lower()
 
+
 def vegetUpper(name):
     return name.upper()
+
 
 def vegetTitle(name):
     return name.title()
 
+
 def sum(count):
     return count
+
 
 vegetable1 = input("1 овощ: ")
 vegetable2 = input("2 овощ: ")
